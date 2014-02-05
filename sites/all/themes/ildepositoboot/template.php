628772408ab4c6867722801a3a90f8fbf0b47de9
Sergio Durzu <sergio.durzu@gmail.com>
2014-02-05 19:39:41 +0100

    Primo deploy tema ilDepositoBoot

---
+++ sites/all/themes/ildepositoboot/template.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * template.php
+ */
+
+/** 
+ * Include di tutte le funzioni dentro preprocess 
+ */
+require 'preprocess/page.php.inc';
+require 'preprocess/node.php.inc';
+require 'preprocess/field.php.inc';
+require 'preprocess/region.php.inc';
+require 'inc/ildepositoboot.php.inc';
+
+
