4f6333e02edf3d98396a553c76d7aa75929fb56a
Sergio Durzu <sergio.durzu@gmail.com>
2014-02-11 22:11:48 +0100

    Modifiche tema ildepositoboot

--- sites/all/themes/ildepositoboot/templates/node--canto--full.tpl.php
+++ sites/all/themes/ildepositoboot/templates/node--canto--full.tpl.php
@@ -1,108 +1,110 @@
 <div<?php print $attributes; ?>>
-  <?php print $user_picture; ?>
-  <?php print render($title_prefix); ?>
-  <?php if (!$page && $title): ?>
-    <div>
-      <h2<?php print $title_attributes; ?>><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
-    </div>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-  <?php if ($display_submitted): ?>
-    <div class="submitted"><?php print $date; ?> -- <?php print $name; ?></div>
-  <?php endif; ?>  
+	<?php print $user_picture; ?>
+	<?php print render($title_prefix); ?>
+	<?php if (!$page && $title): ?>
+		<div>
+			<h2<?php print $title_attributes; ?>><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+		</div>
+	<?php endif; ?>
+	<?php print render($title_suffix); ?>
+	<?php if ($display_submitted): ?>
+		<div class="submitted"><?php print $date; ?> -- <?php print $name; ?></div>
+	<?php endif; ?>  
 </div>
 <div<?php print $content_attributes; ?>>
 
 
+	<div class="row">
 
-    <?php
-    // We hide the comments and links now so that we can render them later.
+		<div class="col-sm-8">
 
-		if ($foto_autori) {
-			print $foto_autori;
-		}
-    print render($content['field_sezione']);
-    print render($content['field_autore_testo']);
-    print render($content['field_anno']);
-		
-		print $canto_tabs;
-		
-		?>
-	
+			<?php
 
+			if ($foto_autori) {
+				print $foto_autori;
+			}
+			print render($content['field_sezione']);
+			print render($content['field_autore_testo']);
+			print render($content['field_anno']);
 
-
-    <?php if ($field_informazioni): ?>
-		<h2 class="block-title"">Informazioni</h2>
-      <?php print render($content['field_informazioni']); ?>
-	
-    <?php endif; ?>
-
-    <?php if ($field_fonte): ?>
-      <h2 class="block-title"">Indicazioni bibliografiche</h2>
-      <?php print render($content['field_fonte']); ?>
-    <?php endif; ?>
-			
-
-
-
-    <h2 class="block-title">Condividi</h2>
-
-    <?php
-    $block = module_invoke('service_links', 'block_view', 'service_links');
-    print render($block['content']);
-    ?>
-
-    <h2 class="block-title" style="margin-top: 20px;">Strumenti</h2>
-
-      <?php
-      $block = module_invoke('alterator', 'block_view', 'strumenti');
-      print render($block['content']);
-      ?>
-
-
-    <?php
-    $view = views_get_view('check_relazioni');
-    $view->set_display('eventi');
-    $view->set_arguments(array($node->nid));
-    $view->pre_execute();
-    $view->execute();
-    if (count($view->result) > 0) {
-      print '<h2 class="block-title top">La storia cantata</h2>';
-      print $view->preview();
-    }
-    ?>
-
-    <h2 class="block-title top">Scheda del canto</h2>
-    <?php
-    print render($content['field_sezione']);
-    print render($content['field_autore_testo']);
-    print render($content['field_autore_musica']);
-    print render($content['field_anno']);
-    print render($content['field_tags']);
-    print render($content['field_lingua']);
-    print '<div>Inserito da: ' . _print_user($node->uid) . '</div>';
-    ?>
-
-
-    <div id="disclaimer">
-      <div class="title">Note di pubblicazione</div>
-      I diritti del contenuto sono dei rispettivi autori.<br />
-      Lo staff de ilDeposito.org non condivide necessariamente il contenuto,
-      che viene inserito nell'archivio unicamente per il suo valore storico, artistico o culturale (<a href="#">maggiori informazioni</a>).
-
-    </div>
+			print $canto_tabs;
+			?>
 
 
 
 
+			<?php if ($field_informazioni): ?>
+				<h2 class="block-title"">Informazioni</h2>
+				<?php print render($content['field_informazioni']); ?>
+
+			<?php endif; ?>
+
+			<?php if ($field_fonte): ?>
+	      <h2 class="block-title"">Indicazioni bibliografiche</h2>
+				<?php print render($content['field_fonte']); ?>
+			<?php endif; ?>
+
+
+		</div>
+		<div class="col-sm-4">
+
+			<h2 class="block-title">Condividi</h2>
+
+			<?php
+			$block = module_invoke('service_links', 'block_view', 'service_links');
+			print render($block['content']);
+			?>
+
+			<h2 class="block-title" style="margin-top: 20px;">Strumenti</h2>
+
+			<?php
+			$block = module_invoke('alterator', 'block_view', 'strumenti');
+			print render($block['content']);
+			?>
+
+
+			<?php
+			$view = views_get_view('check_relazioni');
+			$view->set_display('eventi');
+			$view->set_arguments(array($node->nid));
+			$view->pre_execute();
+			$view->execute();
+			if (count($view->result) > 0) {
+				print '<h2 class="block-title top">La storia cantata</h2>';
+				print $view->preview();
+			}
+			?>
+
+			<h2 class="block-title top">Scheda del canto</h2>
+			<?php
+			print render($content['field_sezione']);
+			print render($content['field_autore_testo']);
+			print render($content['field_autore_musica']);
+			print render($content['field_anno']);
+			print render($content['field_tags']);
+			print render($content['field_lingua']);
+			print '<div>Inserito da: ' . _print_user($node->uid) . '</div>';
+			?>
+
+
+			<div id="disclaimer">
+				<div class="title">Note di pubblicazione</div>
+				I diritti del contenuto sono dei rispettivi autori.<br />
+				Lo staff de ilDeposito.org non condivide necessariamente il contenuto,
+				che viene inserito nell'archivio unicamente per il suo valore storico, artistico o culturale (<a href="#">maggiori informazioni</a>).
+
+			</div>
+
+		</div>
+
+	</div>
 
   <div class="clearfix" style="clear: left;">
-    <?php if (!empty($content['links'])): ?>
-      <div class="links node-links clearfix"><?php print render($content['links']); ?></div>
-    <?php endif; ?>
+		<?php if (!empty($content['links'])): ?>
+			<div class="links node-links clearfix"><?php print render($content['links']); ?></div>
+		<?php endif; ?>
 
-    <?php print render($content['comments']); ?>
+		<?php print render($content['comments']); ?>
   </div>
 </div>
 
