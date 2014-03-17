117bdb1aba140627aade9e838671775e8fcf718c
Sergio Durzu <sergio.durzu@gmail.com>
2014-03-17 19:38:06 +0100

    Aggiornamento Dismiss

--- sites/all/modules/contrib/dismiss/js/dismiss.js
+++ sites/all/modules/contrib/dismiss/js/dismiss.js
@@ -6,15 +6,19 @@
 (function ($) {
 
   Drupal.behaviors.dismiss = {
-    attach: function(context) {
+    attach: function (context) {
 
-      // Append the Dismiss button to each message box.
-      $('.messages').each(function(){
-        $(this).prepend('<a class="dismiss">X</a>');
+      // Prepend the Dismiss button to each message box.
+      $('.messages').each(function () {
+        var flag = $(this).children().hasClass('dismiss');
+
+        if (!flag) {
+          $(this).prepend('<button class="dismiss"><span class="element-invisible">' + Drupal.t('Close this message.') + '</span></button>');
+        }
       });
 
       // When the Dismiss button is clicked hide this set of messages.
-      $('.dismiss').click(function(){
+      $('.dismiss').click(function () {
         $(this).parent().hide('fast');
       });
 
