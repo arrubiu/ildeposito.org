8639452b7f327fed45c7108da4531fa1b4aa8c54
Sergio Durzu <sergio.durzu@gmail.com>
2015-01-17 08:19:09 +0100

    Update core and modules 2

---
+++ sites/all/themes/ildeposito/templates/calendar-mini.tpl.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @file
+ * Template to display a view as a mini calendar month.
+ * 
+ * @see template_preprocess_calendar_mini.
+ *
+ * $day_names: An array of the day of week names for the table header.
+ * $rows: An array of data for each day of the week.
+ * $view: The view.
+ * $min_date_formatted: The minimum date for this calendar in the format YYYY-MM-DD HH:MM:SS.
+ * $max_date_formatted: The maximum date for this calendar in the format YYYY-MM-DD HH:MM:SS.
+ * 
+ * $show_title: If the title should be displayed. Normally false since the title is incorporated
+ *   into the navigation, but sometimes needed, like in the year view of mini calendars.
+ * 
+ */
+//dsm('Display: '. $display_type .': '. $min_date_formatted .' to '. $max_date_formatted);dsm($day_names);
+$params = array(
+  'view' => $view,
+  'granularity' => 'month',
+  'link' => FALSE,
+);
+
+setlocale(LC_ALL, 'it_IT.utf8', 'it_IT');
+$mese_n = substr($view->argument['date_argument']->argument, 5, 2);
+$nav_title = strtoupper(strftime('%B', mktime(0, 0, 0, $id, 10)));
+
+?>
+<div class="calendar-calendar"><div class="month-view">
+<?php if ($show_title): ?>
+<div class="date-nav-wrapper clear-block">
+  <div class="date-nav">
+    <div class="date-heading">
+      <?php print $nav_title; ?>
+    </div>
+  </div>
+</div> 
+<?php endif; ?> 
+<table class="mini">
+  <thead>
+    <tr>
+      <?php foreach ($day_names as $cell): ?>
+        <th class="<?php print $cell['class']; ?>">
+          <?php print $cell['data']; ?>
+        </th>
+      <?php endforeach; ?>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ((array) $rows as $row): ?>
+      <tr>
+        <?php foreach ($row as $cell): ?>
+          <td id="<?php print $cell['id']; ?>" class="<?php print $cell['class']; ?>">
+            <?php print $cell['data']; ?>
+          </td>
+        <?php endforeach; ?>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+</div></div>
