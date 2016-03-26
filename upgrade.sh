c8a3bdc1bfd11ceae589bededdfbde94f6f33710
Sergio Durzu <sergio.durzu@gmail.com>
2016-03-26 19:19:14 +0100

    Update script

--- upgrade.sh
+++ upgrade.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 drush rr
 drush updb -y
-drush en entityreference_migration -y
-drush en imagecache_token -y
+drush en entityreference_migration imagecache_token ildepositoboot -y
 drush emr
 
-drush dis -y emptyparagraphkiller, htmlpurifier, checklistapi, oauth_common, rss_category, rss_enclosure, site_verify, services, uuid_path, feeds, feeds_tamper, forum, custom_formatters, better_revisions, backup_migrate_dropbox, backup_migrate_sftp, blockcache_alter, block_class,  commitlog, versioncontrol, git_status, versioncontrol_git, date_facets, date_repeat_presets, delta_ui, delta, dismiss, field_label_plurals, flag_lists, hidden_comment, linkit_picker, linkit_views, magic, memcache_status, metatag_facebook, metatag_opengraph, omega_tools, simplenews_rules, trigger, user_picture_field, back_to_top, job_scheduler, varnish_status, print, actions_permissions, ds, references, node_reference, noderefcreate, variable_email, rh_taxonomy, autofloat, rdf
+drush dis -y emptyparagraphkiller, htmlpurifier, checklistapi, oauth_common, rss_category, rss_enclosure, site_verify, services, uuid_path, feeds, feeds_tamper, forum, custom_formatters, better_revisions, backup_migrate_dropbox, backup_migrate_sftp, blockcache_alter, block_class,  commitlog, versioncontrol, git_status, versioncontrol_git, date_facets, date_repeat_presets, delta_ui, delta, dismiss, field_label_plurals, flag_lists, hidden_comment, linkit_picker, linkit_views, magic, memcache_status, metatag_facebook, metatag_opengraph, omega_tools, simplenews_rules, trigger, user_picture_field, back_to_top, job_scheduler, varnish_status, print, actions_permissions, ds, references, node_reference, noderefcreate, variable_email, autofloat
 
 drush dis entityreference_migration -y
+drush dis best_responsive sky bartik ildeposito -y
+drush vset theme_default ildepositoboot -y
 
 if [ $(hostname) == 'portatilinux' ];
 then
