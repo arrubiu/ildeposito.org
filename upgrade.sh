3ac8c54626e6e4b2478fa8e6bcfa64cf131ba6e8
Sergio Durzu <sergio.durzu@gmail.com>
2016-03-26 20:13:29 +0100

    Merged ildeposito_app in ildeposito_utils

--- upgrade.sh
+++ upgrade.sh
@@ -8,6 +8,7 @@ drush emr
 drush dis -y emptyparagraphkiller, htmlpurifier, checklistapi, oauth_common, rss_category, rss_enclosure, site_verify, services, uuid_path, feeds, feeds_tamper, forum, custom_formatters, better_revisions, backup_migrate_dropbox, backup_migrate_sftp, blockcache_alter, block_class,  commitlog, versioncontrol, git_status, versioncontrol_git, date_facets, date_repeat_presets, delta_ui, delta, dismiss, field_label_plurals, flag_lists, hidden_comment, linkit_picker, linkit_views, magic, memcache_status, metatag_facebook, metatag_opengraph, omega_tools, simplenews_rules, trigger, user_picture_field, back_to_top, job_scheduler, varnish_status, print, actions_permissions, ds, references, node_reference, noderefcreate, variable_email, autofloat, entityreference_migration
 
 ## Moduli custom
+drush en ildeposito_utils -y
 drush dis -y ildeposito_purge, stampa, ildeposito_news, ildeposito_social, ildeposito_cron, alterator, elenchi, ildeposito_app
 drush pm-uninstall -y ildeposito_purge, stampa, ildeposito_news, ildeposito_social, ildeposito_cron, alterator, elenchi, ildeposito_app
 
