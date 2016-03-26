ccb71810141fcc9f60c2c45700505ab8f69642ee
Sergio Durzu <sergio.durzu@gmail.com>
2016-03-26 11:15:48 +0100

    Added ildepositoboot theme

--- sites/all/themes/ildepositoboot/template.php
+++ sites/all/themes/ildepositoboot/template.php
@@ -1,41 +1,11 @@
 <?php
-
 /**
  * @file
- * template.php
+ * The primary PHP file for this theme.
  */
 
-/** 
- * Include di tutte le funzioni dentro preprocess 
- */
-require 'preprocess/page.php.inc';
-require 'preprocess/node.php.inc';
-require 'preprocess/field.php.inc';
-require 'preprocess/region.php.inc';
-require 'inc/ildepositoboot.php.inc';
+include_once ('includes/ildeposito_preprocess_field.inc');
+include_once ('includes/ildeposito_preprocess_node.inc');
+include_once ('includes/ildeposito_preprocess_page.inc');
+include_once ('includes/ildeposito_preprocess_html.inc');
 
-
-/**
- * Implements hook_facetapi_deactivate_widget
- * @param type $variables
- * @return string
- */
-function ildepositoboot_facetapi_deactivate_widget($vars) {
-	$sep = '<img src="' . $GLOBALS['base_path'] . '' . drupal_get_path('theme', 'ildepositoboot') . '/images/facet_uncheck.png" alt="-" title="Elimina filtro" class="icon" />';
-	return $sep;
-}
-
-/**
- * Override of theme_tablesort_indicator().
- *
- * Use our own image versions, so they show up as black and not gray on gray.
- */
-function ildepositoboot_tablesort_indicator($variables) {
-	$style = $variables['style'];
-	$theme_path = drupal_get_path('theme', 'ildepositoboot');
-	if ($style == 'asc') {
-		return theme('image', array('path' => $theme_path . '/images/facet_su.png', 'alt' => t('sort ascending'), 'title' => t('sort ascending')));
-	} else {
-		return theme('image', array('path' => $theme_path . '/images/facet_giu.png', 'alt' => t('sort descending'), 'title' => t('sort descending')));
-	}
-}
--- sites/all/themes/ildepositoboot/templates/zone--content.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php if ($wrapper): ?><div<?php print $attributes; ?>><?php endif; ?>  
-  <div<?php print $content_attributes; ?>>    
-    <?php if ($breadcrumb): ?>
-      <div id="breadcrumb" class="grid-<?php print $columns; ?>"><?php print $breadcrumb; ?></div>
-    <?php endif; ?>    
-   
-    <?php print $content; ?>
-  </div>
-<?php if ($wrapper): ?></div><?php endif; ?>
