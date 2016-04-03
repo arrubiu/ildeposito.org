e6e0cd6bd8a71c27e11cb1752c1822e4190fec65
Sergio Durzu <sergio.durzu@gmail.com>
2016-04-03 09:35:17 +0200

    Moved custom features

--- upgrade.sh
+++ upgrade.sh
@@ -15,15 +15,15 @@ then
     drush vset eu_cookie_compliance_domain '' -y
 fi
 
-drush en entityreference_migration paragraphs ildeposito_utils ildeposito_libreria imagecache_token ildepositoboot ildeposito_autore ildeposito_customcontent ildeposito_canto ildeposito_articolo -y
+drush en entityreference_migration paragraphs ildeposito_utils ildeposito_libreria imagecache_token ildepositoboot ildeposito_autore ildeposito_customcontent ildeposito_canto ildeposito_articolo ildeposito_evento ildeposito_pagina ildeposito_traduzione -y
 drush emr
 
 ## Moduli contrib
 drush dis -y emptyparagraphkiller, htmlpurifier, checklistapi, oauth_common, rss_category, rss_enclosure, site_verify, services, uuid_path, feeds, feeds_tamper, forum, custom_formatters, better_revisions, backup_migrate_dropbox, backup_migrate_sftp, blockcache_alter, block_class,  commitlog, versioncontrol, git_status, versioncontrol_git, date_facets, date_repeat_presets, delta_ui, delta, dismiss, field_label_plurals, flag_lists, hidden_comment, linkit_picker, linkit_views, magic, memcache_status, metatag_facebook, metatag_opengraph, omega_tools, simplenews_rules, trigger, user_picture_field, back_to_top, job_scheduler, varnish_status, print, actions_permissions, ds, references, node_reference, noderefcreate, variable_email, autofloat, entityreference_migration, speedy, simplehtmldom, xmlsitemap_custom, xmlsitemap_engines, service_links, general_services, widget_services, widgets_service_links, widgets, xmlsitemap_custom, xmlsitemap_engines
 
 ## Moduli custom
-drush dis -y ildeposito_purge, stampa, ildeposito_news, ildeposito_social, ildeposito_cron, alterator, elenchi, ildeposito_app, ildeposito_ct_libreria, ildeposito_ct_canto, ildeposito_ct_customcontent, ildeposito_ct_autore, ildeposito_ct_articolo
-
+drush dis -y ildeposito_purge, stampa, ildeposito_news, ildeposito_social, ildeposito_cron, alterator, elenchi, ildeposito_app, ildeposito_ct_libreria, ildeposito_ct_canto, ildeposito_ct_customcontent, ildeposito_ct_autore, ildeposito_ct_articolo, ildeposito_ct_evento, ildeposito_ct_fonte, ildeposito_ct_pagina, ildeposito_ct_traduzione
+#@TODO: aggiungiugere uninstall per tutti i moduli custom disabilitati quando finito
 
 ## Variabili
 drush vset theme_default ildepositoboot -y
